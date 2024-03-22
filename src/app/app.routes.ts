ef7041deddb90b5a45b1276363a95c41062db569
Eddie <eddie@edease.nl>
2024-03-22 23:40:37 +0100

    basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {AdminComponent} from './admin/admin.component';
+import {LandingComponent} from './landing/landing.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'admin', component: AdminComponent }
+  ];
+
